db24665662b584fcc5090a2a3b1cc42e737d0776
Raan Nyaana <71020725+Raan010101@users.noreply.github.com>
2025-04-23 14:27:22 +0800

    Completed new task InputOutput

---
+++ HackerRank/InputOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std ;
+
+int main(){
+
+    int sum, a,b,c; 
+
+    cout<<"Enter 3 values: "; 
+
+    cin>>a >>b>>c;
+    sum = a+b+c; 
+
+    cout<<"The sum is :"<< sum<<endl; 
+
+    return 0; 
+    
+}
