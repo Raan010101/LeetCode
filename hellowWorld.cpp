4358b6a9f042a49367a38b2fdf6f45e48b02c7e3
Raan Nyaana <71020725+Raan010101@users.noreply.github.com>
2025-04-23 12:25:48 +0800

    my first program

--- hellowWorld.cpp
+++ hellowWorld.cpp
@@ -1,1 +1,9 @@
-#
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Beginning my Coding Journey in the C++ World!" << endl;
+    cout << "This is a simple C++ program." << endl;
+    return 0; 
+}
