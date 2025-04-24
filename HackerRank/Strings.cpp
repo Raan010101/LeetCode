a65c32472262884328aa08ee412561941a20715b
Raan Nyaana <71020725+Raan010101@users.noreply.github.com>
2025-04-24 09:22:02 +0800

    condtional_statements

---
+++ HackerRank/Strings.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std; 
+
+int main() {
+
+
+    char a;
+    char b; 
+    string g; 
+
+    
+cout << "Enter two characters: ";
+cin >> a >> b;
+
+       
+g = a + b ; 
+
+// cout << lenght.a << length.b ; 
+
+// cout << ""
+          
+
+
+
+//     return 0; 
+
+    
+}
