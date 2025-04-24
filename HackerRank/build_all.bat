d6bfe2a928e0e9d8d67b8da3bf4558fb7560d85d
Raan Nyaana <71020725+Raan010101@users.noreply.github.com>
2025-04-24 12:34:27 +0800

    delete some files and made some changes

--- HackerRank/build_all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Build all C++ files in Code directory to build/Debug folder
-
-powershell -Command "Get-ChildItem -Path 'Code' -Filter '*.cpp' | ForEach-Object { g++ -g $_.FullName -o 'build\\Debug\\' + $_.BaseName + '.exe' }"
-
-echo Build complete.
-pause
