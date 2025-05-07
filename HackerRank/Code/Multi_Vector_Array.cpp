f6fd64a1eccedf0a03aed3a8e48fd5d8bda4c822
Raan Nyaana <71020725+Raan010101@users.noreply.github.com>
2025-05-07 11:07:25 +0800

    added comments

--- HackerRank/Code/Multi_Vector_Array.cpp
+++ HackerRank/Code/Multi_Vector_Array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     cout<< "Enter the size of the vector:"; 
     int size; 
-    cin >> size; 
+    cin >> size; // size = 6  
 
      
     vector<int>numbers; 
@@ -17,7 +17,11 @@ int main() {
         numbers.push_back(number);
     }
 
-    for (int i  = size -1 ; i >= 0 ; i--)
+    for (int i  = size -1 ; i >= 0 ; i--) 
+    //size - = 6-1 = 5,  the index will be index 5, 
+    //because the array start at index 0, 
+    //and if the user inputs 6 for array size the value for the last array is number 5
+    // and not 6. 
     {
         cout<< "The number at index " << i << " is: ";
         cout<<numbers[i]<<endl; 
